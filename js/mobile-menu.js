48ee61cfa593312e00aa32e832b22e0b9c232483
Kateryna Okhotnyk <k.okhotnyk@gmail.com>
2022-05-23 04:28:47 +0300

    mobile-menu javascript added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,7 @@
   const refs = {
     openMenuBtn: document.querySelector(".menu-open-btn"),
     closeMenuBtn: document.querySelector(".menu-close-btn"),
-    menu: document.querySelector(".mob-menu"),
+    menu: document.querySelector(".mobile__menu"),
     body: document.querySelector(".body"),
   };
 
